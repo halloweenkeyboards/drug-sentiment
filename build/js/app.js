94457460592fa0ccac2c841468d9ee0d1675d188
Max McCarty <mvmccart@gmail.com>
2015-07-20 21:06:37 -0400

    quick fix for background-images css issue

--- build/js/app.js
+++ build/js/app.js
@@ -22,61 +22,6 @@
 
     angular
     .module('app.drugs')
-    .controller('Drugs', Drugs);
-
-    Drugs.$inject = ['$scope', '$http'];
-
-    function Drugs($scope, $http) {
-        $scope.title = 'the final report on drug experience sentiment';
-        $scope.drugs = [];
-        $scope.w = $('.container-fluid').width();
-        $scope.h = $('.container-fluid').height();
-
-        angular.element(document).ready(function() {
-            $scope.getDrugs();
-            d3.select(window).on('resize', resize);
-        });
-
-        function resize() {
-            console.log('screen resizd');
-            console.log('screen width: ' + $('.drug-report').width() + ' pixels');
-        }
-
-        $scope.getDrugs = function() {
-            $http.get('/api/drugs')
-            .success(function(data) {
-                var minimumExps = 50;
-                var culledData = [];
-                _.each(data, function(doc) {
-                    if (doc.experiences.length >= minimumExps) {
-                        culledData.push(doc);
-                    }
-                });
-                $scope.drugs = culledData;
-                //$scope.drawD3();
-            })
-            .error(function(data) {
-                console.log('error :' + data);
-            });
-        };
-
-        $scope.meetsMinimum = function(drug) {
-            var minimum = 50;
-            if (drug.experiences.length >= minimum) {
-                return true;
-            } else {
-                return false;
-            }
-        };
-    }
-})();
-
-
-(function() {
-    'use strict';
-
-    angular
-    .module('app.drugs')
     .directive('drugList', function() {
         return {
             templateUrl: 'app/widgets/drug-list.html',
@@ -274,6 +219,61 @@
         };
     });
 })();
+
+(function() {
+    'use strict';
+
+    angular
+    .module('app.drugs')
+    .controller('Drugs', Drugs);
+
+    Drugs.$inject = ['$scope', '$http'];
+
+    function Drugs($scope, $http) {
+        $scope.title = 'the final report on drug experience sentiment';
+        $scope.drugs = [];
+        $scope.w = $('.container-fluid').width();
+        $scope.h = $('.container-fluid').height();
+
+        angular.element(document).ready(function() {
+            $scope.getDrugs();
+            d3.select(window).on('resize', resize);
+        });
+
+        function resize() {
+            console.log('screen resizd');
+            console.log('screen width: ' + $('.drug-report').width() + ' pixels');
+        }
+
+        $scope.getDrugs = function() {
+            $http.get('/api/drugs')
+            .success(function(data) {
+                var minimumExps = 50;
+                var culledData = [];
+                _.each(data, function(doc) {
+                    if (doc.experiences.length >= minimumExps) {
+                        culledData.push(doc);
+                    }
+                });
+                $scope.drugs = culledData;
+                //$scope.drawD3();
+            })
+            .error(function(data) {
+                console.log('error :' + data);
+            });
+        };
+
+        $scope.meetsMinimum = function(drug) {
+            var minimum = 50;
+            if (drug.experiences.length >= minimum) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+    }
+})();
+
 
 angular.module("app").run(["$templateCache", function($templateCache) {$templateCache.put("app/widgets/drug-list.html","<div ng-repeat=\"drug in drugs\" class=drug-report ng-attr-id={{drug.cssId}} data-stellar-background-ratio=0.5><drug-report></drug-report></div>");
 $templateCache.put("app/widgets/drug-report.html","<svg ng-attr-width={{w}} ng-attr-height={{h}} ng-init=drugRender(drug)></svg>");}]);
